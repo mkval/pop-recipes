71f9875ae20712ca6aab41d879b06e0ae55df996
Mark Valles <mkvalles@gmail.com>
2026-03-19 04:39:12 +0800

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Mark Valles on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
